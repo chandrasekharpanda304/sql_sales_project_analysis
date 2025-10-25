ab3870c4f31777fe08f0b3e5a24fff6ba7a72de1
Chandrasekhar Panda <chandrasekharpanda304@gmail.com>
2025-10-25 23:05:29 +0530

    Sql_sales_project_analysis.sql

--- Sql_sales_project_analysis.sql
+++ Sql_sales_project_analysis.sql
@@ -23,29 +23,8 @@ SELECT * FROM shop_sales
 
 
 -- Data Cleaning
-
-
-SELECT * FROM shop_sales 
-    WHERE 
-    transaction_id IS NULL
-    OR
-    sale_date IS NULL
-    OR 
-    sale_time IS NULL
-    OR
-    gender IS NULL
-	OR
-	age IS NULL
-    OR
-    category IS NULL
-    OR
-    quantity IS NULL
-    OR
-    cogs IS NULL
-    OR
-    total_sale IS NULL;
     
--- data Ddeleyion of null values--
+-- data Ddeletion of null values--
 
 DELETE FROM shop_sales
     WHERE 
@@ -85,19 +64,6 @@ SELECT DISTINCT category FROM shop_sales
 -- Data Analysis 
 
 -- Q&A
-
--- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05
--- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 10 in the month of Nov-2022
--- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
--- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
--- Q.5 Write a SQL query to find all transactions where the total_sale is greater than 1000.
--- Q.6 Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
--- Q.7 Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
--- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
--- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
--- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <=12, Afternoon Between 12 & 17, Evening >17)
-
-
 
  -- Q.1 Write a SQL query to retrieve all columns for sales made on '2023-05-12'
 
@@ -205,6 +171,7 @@ FROM shop_sales
 GROUP BY customer_id,
 ORDER BY total_quantity DESC
 LIMIT 5;
+
 
 
 -- End of project
